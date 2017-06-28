014b78ecc103c866b58be0ff2359c0ff11a289fa
ahudson <ahudson>
2017-06-28 16:07:32 -0500

    Changing memory reference in task.spawn, fixing syntax

--- upload/RCL.RCL1.js
+++ upload/RCL.RCL1.js
@@ -2,19 +2,23 @@
 class RCLMemoryManage {
     run(rm) {
         //Declare memory
-        Memory.rooms[rm] = {}
-        Memory.rooms[rm].build = {}
+        if(Memory.rooms[rm] == null)
+            Memory.rooms[rm] = {}
+        if(Memory.rooms[rm].build == null)
+            Memory.rooms[rm].build = {}
         Memory.rooms[rm].build.policy = {
             extension: 1,
             container: 0,
         }
-        Memory.rooms[rm].spawn = {}
+        if(Memory.rooms[rm].spawn == null)
+            Memory.rooms[rm].spawn = {}
         Memory.rooms[rm].spawn.worker = {
             chance: 1,
             max: 10,
         }
         //Worker ratios
-        Memory.rooms[rm].worker = {}
+        if(Memory.rooms[rm].worker == null)
+            Memory.rooms[rm].worker = {}
         Memory.rooms[rm].worker.policy = {
             construct: 0.2,
             upgrade: 0.5,
